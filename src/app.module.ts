9847bb324115cc87c1096152469997264c77a4f3
root <hasibul_uap@outlook.com>
2022-08-07 17:10:05 +0600

    users module and controllers add

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'admin',
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import {User} from "./models/user.entity"
+import {TypeOrmModule} from "@nestjs/typeorm"
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User])
+  ],
   controllers: [UserController]
 })
 export class UserModule {}
