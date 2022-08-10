627f06a2434365eb7311ae9709f01c91c358d3d6
root <hasibul_uap@outlook.com>
2022-08-10 16:29:11 +0600

    hashed password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from '../user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Controller()
 export class AuthController {
@@ -10,6 +11,13 @@ export class AuthController {
 
     @Post('register')
     async register(@Body() body) {
-        return await this.userService.create(body);
+        const hashed = await bcrypt.hash(body.password, 12);
+        
+        return await this.userService.create({
+            first_name: body.first_name,
+            last_name: body.last_name,
+            email: body.email,
+            password: hashed
+        });
     }
 }
